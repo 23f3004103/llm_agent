69594cdab7354ec8f3981ba63cb60a6595c4d178
Jems Jayswal <23f3004103@ds.study.iitm.ac.in>
2025-08-27 15:27:34 +0545

    completed

---
+++ app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder=os.path.dirname(os.path.abspath(__file__)))
+
+@app.route('/')
+def index():
+	return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+	return send_from_directory(app.static_folder, path)
+
+if __name__ == '__main__':
+	app.run(debug=True)
